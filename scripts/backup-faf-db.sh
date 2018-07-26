56404c62ec94674a0069707efc35527cdaedc35d
Sheeo <sheeo@sheeo.dk>
2018-07-26 15:37:16 +0200

    Fix backup command and mark script executable

--- scripts/backup-faf-db.sh
+++ scripts/backup-faf-db.sh
@@ -3,4 +3,4 @@
 BACKUP_DIR=/opt/faf/backups/faf-db
 
 mkdir -p ${BACKUP_DIR}
-docker exec -i faf-db mysqldump --single-transaction --triggers --routines --login-path=faf_lobby faf | gzip -c > ${BACKUP_DIR}/$(date +"%Y-%m-%d-%H-%M-%S").sql.gzip
+docker exec -i faf-db mysqldump --login-path=faf_lobby --single-transaction --triggers --routines --all-databases | gzip -c > ${BACKUP_DIR}/$(date +"%Y-%m-%d-%H-%M-%S").sql.gzip
