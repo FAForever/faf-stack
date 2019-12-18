1b4afe4d425be0c75de8afc691c622a6b5e585b7
Brutus5000 <Brutus5000@gmx.net>
2019-12-18 20:43:25 +0100

    Update backup script with production one

--- scripts/backup-faf-db.sh
+++ scripts/backup-faf-db.sh
@@ -1,6 +1,25 @@
 #!/usr/bin/env bash
+# This scripts creates a rotating backup of the last 7 days.
+# Subfolder 01 contains the newewst, subfolder 07 the latest backup.
 
-BACKUP_DIR=/opt/faf/backups/faf-db
+# modify the following to suit your environment
+export DB_BACKUP="/opt/faf/backups/faf-db"
 
-mkdir -p ${BACKUP_DIR}
-docker exec -i faf-db mysqldump --login-path=faf_lobby --single-transaction --triggers --routines --all-databases | gzip -c > ${BACKUP_DIR}/$(date +"%Y-%m-%d-%H-%M-%S").sql.gzip
+echo "*** MySQL backup"
+echo "* Rotate existing backups"
+echo "------------------------"
+
+rm -rf $DB_BACKUP/07
+mv $DB_BACKUP/06 $DB_BACKUP/07
+mv $DB_BACKUP/05 $DB_BACKUP/06
+mv $DB_BACKUP/04 $DB_BACKUP/05
+mv $DB_BACKUP/03 $DB_BACKUP/04
+mv $DB_BACKUP/02 $DB_BACKUP/03
+mv $DB_BACKUP/01 $DB_BACKUP/02
+mkdir $DB_BACKUP/01
+
+echo "* Creating backup..."
+echo "------------------------"
+docker exec -i faf-db mysqldump --login-path=faf_lobby --single-transaction --triggers --routines --all-databases | bzip2  > ${DB_BACKUP}/01/$(date +"%Y-%m-%d-%H-%M-%S").sql.bz2
+echo "Done"
+exit 0
