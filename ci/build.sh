817b07fa7fc13ad8221647f48140600ffd8bc156
Brutus5000 <brutus5000@gmx.net>
2020-01-24 22:36:53 +0100

    Move Travis CI pipeline into improved build script

---
+++ ci/build.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+if [ -z $CI ]; then
+    echo "This script is only intended for Travis CI use. Do not run it on production. IT WILL DELETE YOUR DATABASE."
+    exit 1
+fi
+
+cp -r config.template config
+cp .env.template .env
+wget "https://raw.githubusercontent.com/FAForever/db/$(grep -oP 'faforever/faf-db-migrations:\K(.*)$' ./docker-compose.yml)/test-data.sql"
+
+scripts/init-db.sh
+MAX_WAIT=360 # max. 5 minute waiting time in loop before timeout
+
+docker-compose up -d faf-traefik
+docker-compose up -d faf-java-api
+docker-compose logs -f faf-java-api &
+log_process_id=$!
+
+echo "Waiting for faf-java-api"
+current_wait=0
+while ! curl -s --max-time 1 http://localhost:8010 > /dev/null 2>&1
+do
+  if [ ${current_wait} -ge ${MAX_WAIT} ]; then
+    echo "Timeout on startup of faf-java-api"
+    kill -TERM ${log_process_id}
+    exit 1
+  fi
+  current_wait=$((current_wait+1))
+  sleep 1
+done
+
+kill -TERM ${log_process_id}
+
+echo "Creating test-data in faf-db"
+docker exec -i faf-db mysql -uroot -pbanana faf < test-data.sql
+
+docker-compose up -d faf-website
+docker-compose logs -f faf-website &
+log_process_id=$!
+
+echo "Waiting for faf-website"
+current_wait=0
+while ! curl -s --max-time 1 http://localhost:8020 >/dev/null 2>&1
+do
+  if [ ${current_wait} -ge ${MAX_WAIT} ]; then
+    echo "Timeout on startup of faf-website"
+    kill -TERM ${log_process_id}
+    exit 1
+  fi
+  current_wait=$((current_wait+1))
+  sleep 1
+done
+
+kill -TERM ${log_process_id}
+
+echo "Running test collection"
+docker run --network="host" -t postman/newman:alpine run "https://raw.githubusercontent.com/FAForever/faf-stack/$(urlencode "$TRAVIS_BRANCH")/tests/postman-collection.json"
