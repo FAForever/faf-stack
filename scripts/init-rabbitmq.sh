dd69a998906e307a2a6e599f4c561c2c21265106
Brutus5000 <Brutus5000@gmx.net>
2020-09-12 15:58:36 +0200

    Add new rabbitmq vhost + users for faf core features

--- scripts/init-rabbitmq.sh
+++ scripts/init-rabbitmq.sh
@@ -13,6 +13,13 @@ docker-compose up -d faf-rabbitmq
 
 # Create RabbitMQ users
 docker-compose exec faf-rabbitmq rabbitmqctl wait --timeout ${MAX_WAIT} "${RABBITMQ_PID_FILE}"
+
+docker-compose exec faf-rabbitmq rabbitmqctl add_vhost "${RABBITMQ_FAF_VHOST}"
+docker-compose exec faf-rabbitmq rabbitmqctl add_user "${RABBITMQ_FAF_LOBBY_USER}" "${RABBITMQ_FAF_LOBBY_PASS}"
+docker-compose exec faf-rabbitmq rabbitmqctl set_permissions -p "${RABBITMQ_FAF_VHOST}" "${RABBITMQ_FAF_LOBBY_USER}" ".*" ".*" ".*"
+docker-compose exec faf-rabbitmq rabbitmqctl add_user "${RABBITMQ_FAF_API_USER}" "${RABBITMQ_FAF_API_PASS}"
+docker-compose exec faf-rabbitmq rabbitmqctl set_permissions -p "${RABBITMQ_FAF_VHOST}" "${RABBITMQ_FAF_API_USER}" ".*" ".*" ".*"
+
 docker-compose exec faf-rabbitmq rabbitmqctl add_vhost "${RABBITMQ_POSTAL_VHOST}"
 docker-compose exec faf-rabbitmq rabbitmqctl add_user "${RABBITMQ_POSTAL_USER}" "${RABBITMQ_POSTAL_PASS}"
 docker-compose exec faf-rabbitmq rabbitmqctl set_permissions -p "${RABBITMQ_POSTAL_VHOST}" "${RABBITMQ_POSTAL_USER}" ".*" ".*" ".*"
