1cea2faf21ae0c2b4970d7f0fdc8ee7c4c936ae2
Brutus5000 <Brutus5000@gmx.net>
2023-01-16 19:50:00 +0100

    Update to MongoDB 6.x

--- scripts/init-mongodb.sh
+++ scripts/init-mongodb.sh
@@ -3,6 +3,10 @@
 # fail on errors
 set -e
 
+. config/extra/mongodb/mongodb.env
+
+alias mongo-exec='docker-compose -f faf-extra.yml exec -T mongodb mongosh -u ${MONGO_INITDB_ROOT_USERNAME} -p "${MONGO_INITDB_ROOT_PASSWORD}" --quiet'
+
 if [ ! -f faf-extra.yml ]; then
     echo "You are not inside faf-stack. The working directory must be the root of faf-stack."
     exit 1
@@ -16,7 +20,7 @@ log_process_id=$!
 
 echo "Waiting for mongodb"
 current_wait=0
-while ! docker-compose -f faf-extra.yml exec -T mongodb mongo --eval 'quit(db.runCommand({ ping: 1 }).ok ? 0 : 2)' >/dev/null 2>&1
+while ! mongo-exec --eval 'quit(db.runCommand({ ping: 1 }).ok ? 0 : 2)' >/dev/null 2>&1
 do
   if [ ${current_wait} -ge ${MAX_WAIT} ]; then
     echo "Timeout on startup of mongodb"
@@ -30,8 +34,7 @@ done
 kill ${log_process_id}
 
 echo "Create mongodb database and user for nodebb"
-. config/extra/mongodb/mongodb.env
-docker-compose -f faf-extra.yml exec -T mongodb mongo -u "${MONGO_INITDB_ROOT_USERNAME}" -p "${MONGO_INITDB_ROOT_PASSWORD}" <<MONGODB_SCRIPT
+mongo-exec <<MONGODB_SCRIPT
     use ${MONGO_NODEBB_DATABASE};
     db.createUser( { user: "${MONGO_NODEBB_USERNAME}", pwd: "${MONGO_NODEBB_PASSWORD}", roles: [ "readWrite" ] } );
     db.grantRolesToUser("${MONGO_NODEBB_USERNAME}",[{ role: "clusterMonitor", db: "admin" }]);
