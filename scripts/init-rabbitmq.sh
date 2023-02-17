d612a713377a7d40c6fc476cedcff9d841288711
Yaniv Aknin <yaniv@aknin.name>
2023-02-17 10:07:00 +0000

    wait for rabbitmq to respond to ping

--- scripts/init-rabbitmq.sh
+++ scripts/init-rabbitmq.sh
@@ -14,6 +14,19 @@ docker-compose up -d faf-rabbitmq
 # Create RabbitMQ users
 docker-compose exec faf-rabbitmq rabbitmqctl wait --timeout ${MAX_WAIT} "${RABBITMQ_PID_FILE}"
 
+echo -n "Waiting for rabbitmq status "
+current_wait=0
+while ! docker-compose exec faf-rabbitmq rabbitmqctl status >/dev/null 2>&1
+do
+  if [ ${current_wait} -ge ${MAX_WAIT} ]; then
+    echo "Timeout on startup of rabbitmq"
+    exit 1
+  fi
+  current_wait=$((current_wait+1))
+  sleep 1
+done
+echo ok
+
 docker-compose exec faf-rabbitmq rabbitmqctl add_vhost "${RABBITMQ_FAF_VHOST}"
 docker-compose exec faf-rabbitmq rabbitmqctl add_user "${RABBITMQ_FAF_LOBBY_USER}" "${RABBITMQ_FAF_LOBBY_PASS}"
 docker-compose exec faf-rabbitmq rabbitmqctl set_permissions -p "${RABBITMQ_FAF_VHOST}" "${RABBITMQ_FAF_LOBBY_USER}" ".*" ".*" ".*"
