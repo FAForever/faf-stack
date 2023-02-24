d23496d92e213a7563aaed5fa584be97d2c65cbb
Yaniv Aknin <yaniv@aknin.name>
2023-02-24 08:32:49 +0000

    add logging
    Note I also fixed a potential NameError under the 'if send.returncode'
    branch. Will run the whole file through a linter.

--- scripts/backup-mysql.py
+++ scripts/backup-mysql.py
@@ -2,6 +2,7 @@ import argparse
 import configparser
 import contextlib
 import functools
+import logging
 import pathlib
 import re
 import subprocess
@@ -17,6 +18,7 @@ def zfs(args, timeout=10, stdout=subprocess.PIPE, **kwargs):
 
 def parse_arguments(argv):
     parser = argparse.ArgumentParser()
+    parser.add_argument('--log_file')
     parser.add_argument('mysql_config', help='path to mysql.cnf for snapshotted database', type=argparse.FileType('r'))
     parser.add_argument('snapshot_name', help='zfs dataset name for the snapshotted database data directory')
     parser.add_argument('send_file', help='filename for where zfs-send output should be written',
@@ -35,29 +37,58 @@ def frozen_database(conn):
             yield
             cursor.execute("BACKUP STAGE END;")
 
-def main(options):
-    config = configparser.ConfigParser()
-    with options.mysql_config:
-        config.read_file(options.mysql_config)
-    kwargs = {k: config['client'][k] for k in ('user', 'password', 'host')}
+def setup_logging(options):
+    if options.log_file:
+        logging.basicConfig(filename=options.log_file)
+    elif sys.stderr.isatty():
+        logging.basicConfig(level=logging.INFO)
+    logging.basicConfig()
 
+@contextlib.contextmanager
+def logged_step(message):
+    logging.info(message)
     try:
-        zfs(["destroy", options.snapshot_name])
+        yield
+    except KeyboardInterrupt:
+        logging.warning("interrupted!")
+        raise SystemExit(1)
     except subprocess.CalledProcessError as error:
-        if b'could not find any snapshots to destroy' not in error.stderr:
-            raise
+        logging.error(f"{error.cmd} failed ({error.returncode}); stderr: {error.stderr}")
+        raise SystemExit(1)
+    except Exception as error:
+        logging.exception("step failed")
+        raise
 
-    conn = pymysql.Connection(**kwargs)
-    with frozen_database(conn):
-        zfs(["snapshot", options.snapshot_name])
+def main(options):
+    with logged_step("reading MySQL config"):
+        config = configparser.ConfigParser()
+        with options.mysql_config:
+            config.read_file(options.mysql_config)
+        kwargs = {k: config['client'][k] for k in ('user', 'password', 'host')}
 
-    send = subprocess.Popen(["zfs", "send", options.snapshot_name], stdout=subprocess.PIPE)
-    zstd = subprocess.Popen(["zstd", "-10", "-T4"], stdout=options.send_file, stdin=send.stdout)
-    send.wait()
-    if send.returncode != 0:
-        raise CalledProcessError(send.returncode, "zfs send")
-    zstd.wait()
+    with logged_step("destroying previous snapshot, if any"):
+        try:
+            zfs(["destroy", options.snapshot_name])
+        except subprocess.CalledProcessError as error:
+            if b'could not find any snapshots to destroy' not in error.stderr:
+                raise
+    
+    with logged_step("connecting to database"):
+        conn = pymysql.Connection(**kwargs)
 
+    with logged_step("freezing database and taking snapshot"):
+        with frozen_database(conn):
+            zfs(["snapshot", options.snapshot_name])
+
+    with logged_step("dumping compressed zfs send"):
+        send = subprocess.Popen(["zfs", "send", options.snapshot_name], stdout=subprocess.PIPE)
+        zstd = subprocess.Popen(["zstd", "-10", "-T4"], stdout=options.send_file, stdin=send.stdout)
+        send.wait()
+        if send.returncode != 0:
+            raise subprocess.CalledProcessError(send.returncode, "zfs send")
+        zstd.wait()
 
 if __name__ == '__main__':
-    main(parse_arguments(sys.argv))
+    options = parse_arguments(sys.argv)
+    setup_logging(options)
+    main(options)
